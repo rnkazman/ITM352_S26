ddd53179a67da84504a4c712e8d4e7d8ffdf9ae0
Rick Kazman <kazman@hawaii.edu>
2026-02-19 13:12:12 -1000

    Loops lab

---
+++ Lab7/Ex2a.py
@@ -0,0 +1,9 @@
+evens = []
+
+num = 2
+while (num <= 50):
+    evens.append(num)
+    num += 2
+
+print(evens)
+    
---
+++ Lab7/Ex2b.py
@@ -0,0 +1,9 @@
+evens = [2]
+
+num = 2
+while (evens[-1] < 50):
+    evens.append(num*2)
+    num += 1
+
+print(evens)
+    
